fd59b417a6eecdb6d2256e30a5016648c4659609
andreisoroka <unickpro@gmail.com>
2022-08-19 16:29:00 +0300

    update

--- service-a/src/views/B3View.vue
+++ service-a/src/views/B3View.vue
@@ -1,7 +1,13 @@
 <script setup>
+// other way don't work too:
+// import { defineAsyncComponent } from 'vue';
+// // eslint-disable-next-line import/no-unresolved
+// const HomeView = defineAsyncComponent(() => import('b3/HomeView.vue'));
+
 // eslint-disable-next-line import/no-unresolved
 import HomeView from 'b3/HomeView.vue';
 </script>
+
 <template>
   b3
  <HomeView/>
